d9129589b03ae1760a300f173b515b6806b35147
gao <gabrillo177@gmail.com>
2025-03-22 02:42:20 +0100

    add logout api, do the test, works well

--- src/main/java/com/yugao/service/UserService.java
+++ src/main/java/com/yugao/service/UserService.java
@@ -1,24 +1,21 @@
 package com.yugao.service;
 
 import com.yugao.domain.User;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
 
 public interface UserService {
 
 
-    public User getUserById(int Id);
+    User getUserById(int Id);
 
-    public User getUserByName(String username);
+    User getUserByName(String username);
 
-    public User getUserByEmail(String email);
+    User getUserByEmail(String email);
 
-    public boolean  addUser(User user);
+    boolean  addUser(User user);
 
-    public boolean  updateStatus(int id, int status);
+    boolean  updateStatus(int id, int status);
 
-    public boolean  updateHeader(int id, String headerUrl);
+    boolean  updateHeader(int id, String headerUrl);
 
-    public boolean  updatePassword(int id, String password);
+    boolean  updatePassword(int id, String password);
 }
